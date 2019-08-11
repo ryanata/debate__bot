001e85f74f9294091da83b26a20d416bacd87717
Ryan Ata <44108244+ryanata@users.noreply.github.com>
2019-08-10 22:37:03 -0400

    Installed gspread to store data

--- reddit_bot.py
+++ reddit_bot.py
@@ -1,12 +1,30 @@
 import praw
 from bot_values import client_id, client_secret, username, password, user_agent
 from datetime import datetime
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import operator
+
+'''
+This program uses two main apis- Google Sheets and Reddit praw. A google sheet is updated with every report that runs through
+the program. 
+'''
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+creds= ServiceAccountCredentials.from_json_keyfile_name("Debate Trade Bot-50c9eb8adba7.json",scope)
+
+client = gspread.authorize(creds)
+
+sheet_one= client.open('debateBot').sheet1
+sheet_two=client.open('debateBot').get_worksheet(1)
 
 
 reddit= praw.Reddit(client_id=client_id,client_secret=client_secret,username=username,password=password,user_agent=user_agent)
 
 user_reports= {}
 reporters={}
+
 reply_template = '[Github](https://github.com/ryanata/debate__bot)'
 bottom_text=f'\n\n ______________________________\n\n^(this is a bot. more info can be retrieved here : ) ^{reply_template} '
 
@@ -15,7 +33,15 @@ def report(user):
 		user_reports[user]+=1
 	except KeyError:
 		user_reports[user]=1
-	# this is where i plan to update a google sheet :)
+	
+def update_sheet():
+	sheet_two.resize(rows=1)
+	sheet_two.resize(rows=4000)
+	sorted_reports = sorted(user_reports.items(), key=operator.itemgetter(1))
+	for x,y in sorted_reports:
+		sheet_two.insert_row([x,y],2)
+		
+
 
 def timediff(utc):
 	then = datetime.fromtimestamp(utc)
@@ -50,13 +76,16 @@ def user_stats(user):
 
 	return (f'User {user} has {posts} posts in the /r/debatetrade server and {reports} reports. Their account was created {int(timediff(user.created_utc))} days ago')
 
-subreddit = reddit.subreddit('debatetrade')
+subreddit = reddit.subreddit('waterdebate47')
 
 
 
 stream = praw.models.util.stream_generator(lambda **kwargs: submissions_and_comments(subreddit, **kwargs))
 
 for post in stream:
+	'''
+	Updated stats that is auto-commented with every submission
+	'''
 	if type(post).__name__ == 'Submission' and post.author is not None:
 
 		user= post.author #instance of redditor class
@@ -69,8 +98,9 @@ for post in stream:
 			out=(user_stats(user))
 			post.reply(f'{out}{bottom_text}')
 			print(out)
-		
+	
 	elif type(post).__name__ == 'Comment' and post.author is not None:
+
 		post.refresh()
 		comment_tree=list(map(lambda c: c.author.name if c.author is not None else 'ha', post.replies))
 		message= str(post.body).split()
@@ -114,13 +144,17 @@ for post in stream:
 				print(f'User {z.name} has been officially reported. Their number of reports is: {user_reports[z.name]}')
 				if not commented:
 					post.reply(f'User {z.name} has been officially reported. Their number of reports is: {user_reports[z.name]}{bottom_text}')
+				sheet_one.insert_row([str(post.author),str(post.body)],2)
 				reporters[post.author]=[z.name] #the commentor now has a value in the dictionary with index 0 being the most recently reported person
+				update_sheet()
 			elif real and z.name not in reporters[post.author]: # checks to see if the commenter's target was already reported by the commenter
 				report(z.name)
 				print(f'User {z.name} has been officially reported. Their number of reports is: {user_reports[z.name]}')
 				if not commented:
 					post.reply(f'User {z.name} has been officially reported. Their number of reports is: {user_reports[z.name]}{bottom_text}')
-				reporters[post.author].append(z.name) 
+				sheet_one.insert_row([str(post.author),str(post.body)],2)
+				reporters[post.author].append(z.name)
+				update_sheet()
 			elif real:
 				print(f'Sorry, {post.author}, you have already reported {z.name}!')
 				if not commented:
